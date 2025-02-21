f03c64e1720dbceceb52779cce4880a1c84f2c8f
SharonMawiaJohn <sharonmawia76@gmail.com>
2025-02-21 12:00:00 +0300

    Update

--- Question1.py
+++ Question1.py
@@ -1,3 +1,3 @@
 #Alice has some cards that are facing downwards.
 #The idea is to find a specific card by turning over a minimum number of cards.
-#The cards are arranged in a 
+#The cards are arranged in a row and each card 
